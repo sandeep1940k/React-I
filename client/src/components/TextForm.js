a1ce1137dfc050759bf14c7b0edc0b60b7f3afc3
sandeep1940k <sandeepsana2003@gmail.com>
2025-01-09 13:13:10 +0530

    lecture 11

---
+++ client/src/components/TextForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+export default function TextForm({ heading }) {
+    const [text, setText] = useState('');
+    const [word, setWord] = useState(0);
+    const handleText = (event) => {
+        setText(event.target.value )
+        let textCount = event.target.value.split(' ');
+        let word = 0;
+        textCount.forEach(count => {
+            if(count){
+                word++;
+                setWord(word)
+            }else{
+                setWord(word)
+            }
+        })
+    }
+
+    const handleUpClick = () => {
+        setText(text.toUpperCase());
+    }
+    const handleLwClick = () => {
+        setText(text.toLowerCase());
+    }
+    const handleClearClick = (event) => {
+        setText('');
+        handleText(event)
+    }
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(text);
+    }
+    const handleExtraSpaceClick = () => {
+        setText(text.replace(/\s+/g, ' '));
+    }
+
+    return (
+        <>
+        <div className='container'>
+            <h1>{heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleText} placeholder='Enter text here'></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLwClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaceClick}>Extra Space</button>
+        </div>
+        <div className='container my-3'>
+            <h2>Your text summary</h2>
+            <p>{word} words and {text.length} characters</p>
+            <p>{0.008 * text.split(' ').length } Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.toLowerCase()}</p>
+        </div>
+        </>
+    )
+}
